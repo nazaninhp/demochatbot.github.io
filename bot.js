0520b255176f5eae0f7e7bdfa8d6cafdba9fe447
Nazanin Hashemipoor <76796685+nazaninhp@users.noreply.github.com>
2024-01-16 11:06:39 -0500

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,4 @@
+<script type='text/javascript' src='https://c.la3-c1-ia6.salesforceliveagent.com/content/g/js/59.0/deployment.js'></script>
+<script type='text/javascript'>
+liveagent.init('https://d.la3-c1-ia6.salesforceliveagent.com/chat', '572Ho000000DkWw', '00DHo000002f07n');
+</script>
